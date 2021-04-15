d692c626e78a8a0fa99bc5e13b59fa632d0fbedb
t <38640424+poohcid@users.noreply.github.com>
2021-04-15 19:07:32 +0700

    authorization

--- apollo-server/src/graphql/queries/user.js
+++ apollo-server/src/graphql/queries/user.js
@@ -1,5 +1,19 @@
-import { UserTC } from '../../models'
+import { schemaComposer } from 'graphql-compose'
+import { UserTC, UserModel } from '../../models'
 
 export const userById = UserTC.getResolver('findById')
 export const user = UserTC.getResolver('findOne')
 export const users = UserTC.getResolver('findMany')
+
+export const me = schemaComposer.createResolver({
+    name: 'me',
+    type: UserTC.getType(),
+    resolve: async ({ context }) => {
+      if (!context.user) {
+        return null
+      }
+      const { _id } = context.user
+      const user = await UserModel.findById(_id)
+      return user
+    },
+  })
