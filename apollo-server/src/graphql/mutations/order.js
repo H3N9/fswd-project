7c719c13b6c6763a80dfe4a2f8e0616e2ed33af6
t <38640424+poohcid@users.noreply.github.com>
2021-04-20 00:58:21 +0700

    valid setCart

--- apollo-server/src/graphql/mutations/order.js
+++ apollo-server/src/graphql/mutations/order.js
@@ -1,6 +1,6 @@
 import { schemaComposer } from 'graphql-compose'
 
-import { OrderProductModel, OrderTC, OrderModel, OrderPromotionModel } from '../../models'
+import { OrderProductModel, OrderTC, OrderModel, OrderPromotionModel, ProductModel } from '../../models'
 import { authCreateMiddleware } from './middleware'
 
 export const createOrder = OrderTC.getResolver('createOne', [authCreateMiddleware]).removeArg('record')
@@ -35,7 +35,22 @@ export const setCart = schemaComposer.createResolver({
                 order = await OrderModel.create({ userId: user._id })
             }
 
-            const productOrderInput = args.records.map((item1) => ({...item1, orderId: order._id }))
+            //const productOrdersInput = args.records.map((item1) => ({...item1, orderId: order._id }))
+            const productOrderInput = await args.records.reduce(async (acc, curr) => {
+                return acc.then( async (acc) => {
+                    const product = await ProductModel.findById(curr.productId)
+                    if (product?.quantity > 0){
+                        const newObj = {
+                            ...curr,
+                            orderId: order._id,
+                            quantity: Math.min(product.quantity, curr.quantity)
+                        }
+                        return [...acc, newObj]
+                }
+                return acc
+                })
+            }, Promise.resolve([]))
+
             await OrderProductModel.deleteMany({ orderId: order._id })
             await OrderProductModel.insertMany(productOrderInput)
             order.updatedAt = Date.now()
