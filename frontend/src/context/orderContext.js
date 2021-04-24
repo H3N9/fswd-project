48d5265ba15d6b0cab9d413a158dbdc43bdcf6f0
Ramil Arthan <42642459+H3N9@users.noreply.github.com>
2021-04-24 18:45:19 +0700

    Update orderContext.js

--- frontend/src/context/orderContext.js
+++ frontend/src/context/orderContext.js
@@ -11,7 +11,7 @@ export const OrderProvider = (props) => {
     const [orders, setOrders] = useState([])
     const [setCart] = useMutation(SETCART_MUTATION)
     const {user} = useSession()
-    const [loadMyOrder, {loading, data}] = useLazyQuery(MYORDER_QUERY)
+    const [loadMyOrder, {data}] = useLazyQuery(MYORDER_QUERY)
     useEffect(() => {
         const loadCart = async () => {
             try {
