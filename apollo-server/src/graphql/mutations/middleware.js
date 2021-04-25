eaadbcd273e4f620bb54f711aa697a6633f0b9fc
t <38640424+poohcid@users.noreply.github.com>
2021-04-25 13:50:59 +0700

    validate product and createUser

--- apollo-server/src/graphql/mutations/middleware.js
+++ apollo-server/src/graphql/mutations/middleware.js
@@ -1,3 +1,5 @@
+import { ProductModel, UserModel } from '../../models'
+
 export const authCreateMiddleware = async (resolve, source, args, context, info) => {
     if (context?.user) {
         const newArgs = {
@@ -17,6 +19,28 @@ export const adminPermission = async (resolve, source, args, context, info) => {
         return resolve(source, args, context, info)
     }
     throw new Error('You must be authorized');
+}
+
+export const productValid = async (resolve, source, args, context, info) => {
+    const { title } = args.record
+    const productByTitle = await ProductModel.findOne({ title })
+    if (productByTitle){
+        throw new Error('title is already taken')
+    }
+    else{
+        return resolve(source, args, context, info)
+    }
+}
+
+export const createUserValid = async (resolve, source, args, context, info) => {
+    const { username } = args.record
+    const userByUsername = await UserModel.findOne({ username })
+    if (userByUsername){
+        throw new Error('username is already taken')
+    }
+    else{
+        return resolve(source, args, context, info)
+    }
 }
 
 export default authCreateMiddleware
