9909a13733c0f86b6af6d99f3d1e89e9dbb7eb34
t <38640424+poohcid@users.noreply.github.com>
2021-04-28 15:14:15 +0700

    add mutation remove discount product promotion

--- apollo-server/src/graphql/mutations/promotion.js
+++ apollo-server/src/graphql/mutations/promotion.js
@@ -1,5 +1,7 @@
 import { PromotionTC, DiscountPricePromotionTC, CouponPromotionTC } from '../../models'
+import { adminPermission } from './middleware'
 
-export const createDiscountPricePromotion = DiscountPricePromotionTC.getResolver('createOne')
-export const createCouponPromotion = CouponPromotionTC.getResolver('createOne')
-export const updatePromotion = PromotionTC.getResolver('updateById')
+export const createDiscountPricePromotion = DiscountPricePromotionTC.getResolver('createOne', [adminPermission])
+export const createCouponPromotion = CouponPromotionTC.getResolver('createOne', [adminPermission])
+export const updatePromotion = PromotionTC.getResolver('updateById', [adminPermission])
+export const removeDiscountPriceById = DiscountPricePromotionTC.getResolver('removeById', [adminPermission])
