c0958730e8d980ee6f029a3dd7470a465e80b556
arinnaline <niinewz.look@gmail.com>
2021-05-01 23:35:25 +0700

    fix bootstrap
    row col

--- frontend/src/pages/myInfo.js
+++ frontend/src/pages/myInfo.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
-import { useQuery } from '@apollo/client'
+import { from, useQuery } from '@apollo/client'
 import logo from '../images/logo.png'
 import whiteLogo from '../images/logo-white.webp'
 import {Form, MainContainer, LoginContainer, Input, LogoContainer} from '../styles/styleComponents'
@@ -20,80 +20,79 @@ const MyInfo = () => {
     return (
         <Container>
             <div className="container bootstrap snippet col">
-                {/* <div className="row">
-                    <div className="col-sm-5">
-                        <h1> Username</h1>
-                    </div>
-
-                </div> */}
-                {/* <div className="row">
+                {/* <Row>
+                        <div className="col-sm-5">
+                            <h1> Username</h1>
+                        <div>
+                    </Row> */}
+                {/* <Row>
                     <div className="col-sm-3">
                         <div className="text-center">
-                            <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" className="avatar img-circle img-thumbnail"
+                            <img src="http://ssl.gstatic.com/accounts/ui/avatar_2x.png" roundedCircle
                                 alt="avatar" />
                             <h6>Upload a your photo</h6>
                             <input type="file" className="text-center center-block file-upload" />
                         </div>
                         <br></br>
                     </div>
-                </div> */}
+                </Row> */}
                 <div className="col-sm-9">
                    <h2>My profile</h2>
                     <div className="tab-content">
                         <div className="tab-pane active" id="home">
                             <hr></hr>
-                            <form className="form" action="##" method="post" id="profile" >
-                                <div className="form-group">
+                            <Form className="form" action="##" method="post" id="profile">
+                                <Form.Group>
                                     <div className="col-xs-6">
-                                        <label for="first_name">
+                                        <Form.Label for="first_name">
                                             <h4>First name</h4>
-                                        </label>
-                                        <input type="text" className="form-control" name="first_name" id="first_name" placeholder="first name" title="enter your first name if any." />
+                                        </Form.Label>
+                                        <Form.Control type="text" name="first_name" id="first_name" placeholder="first name" title="enter your first name if any." />
                                     </div>
-                                </div>
-                                <div className="form-group">
+                                </Form.Group>
+                                <Form.Group>
                                     <div className="col-xs-6">
-                                        <label for="last_name">
+                                        <Form.Label for="last_name">
                                             <h4>Last name</h4>
-                                        </label>
-                                        <input type="text" className="form-control" name="last_name" id="last_name" placeholder="last name" title="enter your last name if any." />
+                                        </Form.Label>
+                                        <Form.Control type="text" className="form-control" name="last_name" id="last_name" placeholder="last name" title="enter your last name if any." />
                                     </div>
-                                </div>
-                                <div className="form-group">
+                                </Form.Group>
+                                <Form.Group>
                                     <div className="col-xs-6">
-                                        <label for="password">
+                                        <Form.Label for="password">
                                             <h4>old Password</h4>
-                                        </label>
-                                        <input type="password" className="form-control" name="password" id="password"
+                                        </Form.Label>
+                                        <Form.Control type="password" className="form-control" name="password" id="password"
                                             placeholder="password" title="enter your password." />
                                     </div>
-                                </div>
-                                <div className="form-group">
-                                    <div className="col-xs-6">
-                                        <label for="confirm password">
+                                </Form.Group>
+                                <Form.Group>
+                                    <div className="col-xs-6" >
+                                        <Form.Label for="confirm password">
                                             <h4>new Password</h4>
-                                        </label>
-                                        <input type="password" className="form-control" name="c_password" id="c_password"
+                                        </Form.Label>
+                                        <Form.Control type="password" className="form-control" name="c_password" id="c_password"
                                             placeholder="confirm password" title="enter your password again." />
                                     </div>
-                                </div>
-                                <div className="form-group">
-                                    <div className="col-xs-6">
-                                        <label for="confirm password">
+                                </Form.Group>
+                                <Form.Group>
+                                    <div className="col-xs-6" >
+                                        <Form.Label for="confirm password">
                                             <h4>Confirm new Password</h4>
-                                        </label>
-                                        <input type="password" className="form-control" name="c_password" id="c_password"
+                                        </Form.Label>
+                                        <Form.Control type="password" className="form-control" name="c_password" id="c_password"
                                             placeholder="confirm password" title="enter your password again." />
                                     </div>
-                                </div>
-                                <div className="form-group">
+                                </Form.Group>
+                                <Form.Group>
                                     <div className="col-xs-12">
                                         <br></br>
                                         <button className="btn btn-lg btn-success" type="submit"><i className="glyphicon glyphicon-ok-sign"></i> Save</button>
                                         <button className="btn btn-lg" type="reset"><i className="glyphicon glyphicon-repeat"></i>Reset</button>
                                     </div>
-                                </div>
-                            </form>
+                                </Form.Group>
+                            </Form>
                             <hr></hr>
                         </div>
                     </div>
